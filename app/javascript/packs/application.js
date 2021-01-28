5ade184a94651f452b20eb7f6dec3414e6a80ab9
Octavian Neguletu <neguletuoctavian@gmail.com>
2021-01-28 17:18:44 +0000

    Bring in frontent-gov lib

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,11 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("../packs/stylesheets/application")
 
+import { initAll } from 'govuk-frontend';
+
+initAll();
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
