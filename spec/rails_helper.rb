bac479f01e1bdf1451737c56ce63813808b039e4
Octavian Neguletu <neguletuoctavian@gmail.com>
2021-02-16 14:41:24 +0000

    Add simplecov gem for test coverage analysis

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,9 @@ require 'spec_helper'
 require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
 
+require 'simplecov'
+SimpleCov.start
+
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
