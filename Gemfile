2cd3e5e1ddd4b9e38f88915561560cc5656d2a58
William Fish <william.michael.fish@gmail.com>
2021-02-18 10:24:32 +0000

    Adds country behaviour

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'logstash-event'
 gem 'govuk_design_system_formbuilder'
 
 # API client for the trade tariff api
-gem 'uktt', '~> 0.3.1', git: 'https://github.com/TransformCore/uktt.git'
+gem 'uktt', '~> 0.4.0', git: 'https://github.com/TransformCore/uktt.git'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,18 +1,19 @@
-class Commodity
+class Country
   attr_reader :code
 
-  def initialize(code:, service: :uk)
-    @code = code
+  def initialize(service: :uk)
     @service = service
   end
 
-  def description
-    response.data.attributes.description
+  def list
+    @formatted_countries ||= response.data.map { |country|
+      country.attributes.description
+    }.sort
   end
 
   def response
     @response ||= begin
-                    commodity = Uktt::Commodity.new(options)
+                    commodity = Uktt::Country.new(options)
                     commodity.retrieve
                     commodity.response
                   end
@@ -24,6 +25,5 @@ class Commodity
 
   def options
     OptionBuilder.new(service).call
-      .merge(commodity_id: code)
   end
 end
