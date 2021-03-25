47d85d06e91dc2ae740546701af893d8466fe07d
William Fish <william.michael.fish@gmail.com>
2021-03-25 09:16:48 +0000

    Tidies

--- app/controllers/wizard/steps/duty_controller.rb
+++ app/controllers/wizard/steps/duty_controller.rb
@@ -2,7 +2,7 @@ module Wizard
   module Steps
     class DutyController < BaseController
       def show
-        @duty_option = DutyCalculator.new(user_session, filtered_commodity).result
+        @duty_options = DutyCalculator.new(user_session, filtered_commodity).result
       end
     end
   end
