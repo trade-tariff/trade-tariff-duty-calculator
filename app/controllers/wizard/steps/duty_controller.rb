96e67d0ad05ec4322e23b03e1c61d02edcac83db
Octavian Neguletu <neguletuoctavian@gmail.com>
2021-03-31 14:19:59 +0100

    Add exchange rate details partial

--- app/controllers/wizard/steps/duty_controller.rb
+++ app/controllers/wizard/steps/duty_controller.rb
@@ -3,6 +3,7 @@ module Wizard
     class DutyController < BaseController
       def show
         @duty_options = DutyCalculator.new(user_session, filtered_commodity).result
+        @gbp_to_eur_exchange_rate = Api::ExchangeRate.for('GBP').rate
       end
     end
   end
