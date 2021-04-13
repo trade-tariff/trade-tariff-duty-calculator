c3bbec2c4559f33c08d62fef475ee6386b41d1f3
Octavian Neguletu <neguletuoctavian@gmail.com>
2021-04-13 10:53:16 +0100

    Send custom attributes to NewRelic
    We want to know what users have on their
    session so we can easily debug.

--- spec/features/import_date_page_spec.rb
+++ spec/features/import_date_page_spec.rb
@@ -1,6 +1,24 @@
 RSpec.describe 'Import Date Page', type: :feature do
   let(:commodity_code) { '0702000007' }
   let(:referred_service) { 'uk' }
+  let(:tracked_attributes) do
+    {
+      session: session,
+      commodity_code: '0702000007',
+      commodity_source: 'uk',
+      referred_service: 'uk',
+    }
+  end
+
+  let(:session) do
+    {
+      'answers' => {},
+      'commodity_code' => '0702000007',
+      'commodity_source' => 'uk',
+      'referred_service' => 'uk',
+      'session_id' => 'd4c5d128a7b77649cec4508371b0ae8e',
+    }
+  end
 
   it 'does not store an invalid import date on the session' do
     visit import_date_path(commodity_code: commodity_code, referred_service: referred_service)
@@ -25,4 +43,16 @@ RSpec.describe 'Import Date Page', type: :feature do
 
     expect(Capybara.current_session.driver.request.session['answers'][Wizard::Steps::ImportDate.id]).to eq('3000-12-12')
   end
+
+  # rubocop:disable RSpec/AnyInstance
+  it 'does send custom attributes to NewRelic' do
+    allow(::NewRelic::Agent).to receive(:add_custom_attributes)
+    # This is needed as the session_id changes per each test run
+    allow_any_instance_of(ActionDispatch::Request::Session).to receive(:to_h).and_return(session)
+
+    visit import_date_path(commodity_code: commodity_code, referred_service: referred_service)
+
+    expect(::NewRelic::Agent).to have_received(:add_custom_attributes).with(tracked_attributes)
+  end
+  # rubocop:enable RSpec/AnyInstance
 end
