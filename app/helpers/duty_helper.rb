55d9b1b418a142e9c46c1bceb6e66a19f309e6a5
William Fish <william.michael.fish@gmail.com>
2021-04-16 10:41:03 +0100

    DutyHelper unit tests

--- app/helpers/duty_helper.rb
+++ app/helpers/duty_helper.rb
@@ -3,6 +3,8 @@ module DutyHelper
     t('duty_calculations.options.company_warning_text', commodity_link: commodity_link).html_safe if filtered_commodity.company_defensive_measures?
   end
 
+  private
+
   def commodity_link
     link_to('commodity page', commodity_url(commodity_code))
   end
