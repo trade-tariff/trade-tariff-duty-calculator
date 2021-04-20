09e045ba5e713520489861f5443ff8080fbfe108
Octavian Neguletu <neguletuoctavian@gmail.com>
2021-04-20 10:20:58 +0100

    Round the exchange rate to 4 decimals

--- app/controllers/wizard/steps/duty_controller.rb
+++ app/controllers/wizard/steps/duty_controller.rb
@@ -3,7 +3,7 @@ module Wizard
     class DutyController < BaseController
       def show
         @duty_options = DutyCalculator.new(user_session, filtered_commodity).result
-        @gbp_to_eur_exchange_rate = Api::ExchangeRate.for('GBP').rate
+        @gbp_to_eur_exchange_rate = Api::ExchangeRate.for('GBP').rate.round(4)
       end
     end
   end
