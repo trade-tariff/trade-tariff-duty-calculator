0fd2182218b35a48b967336d932390e5e3b0294b
William Fish <william.michael.fish@gmail.com>
2021-05-12 16:23:10 +0100

    Adds customs_value factory

---
+++ spec/factories/steps/customs_value.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :customs_value, class: 'Wizard::Steps::CustomsValue', parent: :step do
+    transient do
+      possible_attributes do
+        {
+          monetary_value: 'monetary_value',
+          insurance_cost: 'insurance_cost',
+          shipping_cost: 'shipping_cost',
+        }
+      end
+    end
+
+    monetary_value { 'monetary_value' }
+    insurance_cost { 'insurance_cost' }
+    shipping_cost { 'shipping_cost' }
+  end
+end
