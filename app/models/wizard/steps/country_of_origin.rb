92b5b1c27206a349da617c656e66a90015cf3ffb
Octavian Neguletu <neguletuoctavian@gmail.com>
2021-06-01 13:13:07 +0100

    Switch commodity source to UK
    If there is a commodity that has zero mfn duties, then
    we need to switch its source to UK in this case, since
    originally the source would be set up to XI on this
    route.

--- app/models/wizard/steps/country_of_origin.rb
+++ app/models/wizard/steps/country_of_origin.rb
@@ -75,9 +75,11 @@ module Wizard
 
       def next_step_for_row_to_ni
         return trade_remedies_path if trade_defence
-        return customs_value_path if zero_mfn_duty
+        return trader_scheme_path unless zero_mfn_duty
 
-        trader_scheme_path
+        user_session.commodity_source = 'uk'
+
+        customs_value_path
       end
 
       def next_step_for_row_to_gb
