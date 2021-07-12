a7b0aca4bfe9a09b3bccc8e4bcc604c366fcda7f
cacquani <cacquani@mail.com>
2021-07-12 14:11:22 +0100

    HOTT-791: add proper favicon

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -1,3 +1,6 @@
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
+
 require.context('govuk-frontend/govuk/assets');
 
 import '../styles/application.scss';
