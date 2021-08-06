242e3d23f0c4e78248ef2e5988b721024469e4d1
William Fish <william.michael.fish@gmail.com>
2021-08-06 15:35:59 +0100

    Code review

--- app/helpers/excise_helper.rb
+++ app/helpers/excise_helper.rb
@@ -1,20 +1,18 @@
 module ExciseHelper
-  def sanitized_excise_hint
-    hint_text = t(
+  def excise_hint(small_brewers_relief:)
+    extra_brewers_hint = small_brewers_relief ? small_brewers_relief_hint_text : nil
+
+    t(
       'excise_page.hint_html',
-      small_brewers_relief_hint_text: small_brewers_relief_hint_text,
+      small_brewers_relief_hint_text: extra_brewers_hint,
       excise_link: excise_link,
     )
-
-    sanitize(hint_text, attributes: %w[href target class])
   end
 
   private
 
   def small_brewers_relief_hint_text
-    # rubocop:disable Rails/HelperInstanceVariable
-    t('excise_page.small_brewers_relief_hint_text_html', small_brewers_relief_link: small_brewers_relief_link) if @step.small_brewers_relief?
-    # rubocop:enable Rails/HelperInstanceVariable
+    t('excise_page.small_brewers_relief_hint_text_html', small_brewers_relief_link: small_brewers_relief_link)
   end
 
   def excise_link
@@ -23,7 +21,7 @@ module ExciseHelper
       'https://www.gov.uk/government/publications/uk-trade-tariff-excise-duties-reliefs-drawbacks-and-allowances/uk-trade-tariff-excise-duties-reliefs-drawbacks-and-allowances',
       class: 'govuk-link',
       target: '_blank',
-      rel: 'noopener',
+      rel: 'noopener norefferer',
     )
   end
 
@@ -33,7 +31,7 @@ module ExciseHelper
       'https://www.gov.uk/government/publications/excise-notice-226-beer-duty/excise-notice-226-beer-duty--2#small-brewery-beer',
       class: 'govuk-link',
       target: '_blank',
-      rel: 'noopener',
+      rel: 'noopener norefferer',
     )
   end
 end
--- spec/helpers/excise_helper_spec.rb
+++ spec/helpers/excise_helper_spec.rb
@@ -1,21 +1,19 @@
 RSpec.describe ExciseHelper do
   describe '#sanitized_excise_hint' do
-    before { assign(:step, step) }
-
-    let(:step) { instance_double('Steps::Excise', small_brewers_relief?: small_brewers_relief) }
+    subject(:excise_hint) { helper.excise_hint(small_brewers_relief: small_brewers_relief) }
 
     context 'when the excise additional codes do not have small brewers relief' do
       let(:small_brewers_relief) { false }
 
-      it { expect(helper.sanitized_excise_hint).to be_html_safe }
-      it { expect(helper.sanitized_excise_hint).not_to match(/SBR/) }
+      it { is_expected.to be_html_safe }
+      it { is_expected.not_to match(/SBR/) }
     end
 
     context 'when the excise additional codes have small brewers relief' do
       let(:small_brewers_relief) { true }
 
-      it { expect(helper.sanitized_excise_hint).to be_html_safe }
-      it { expect(helper.sanitized_excise_hint).to match(/SBR/) }
+      it { is_expected.to be_html_safe }
+      it { is_expected.to match(/SBR/) }
     end
   end
 end
