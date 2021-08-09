3379f8a8d55b2ce208a934b7183bbdda97085000
William Fish <william.michael.fish@gmail.com>
2021-08-09 16:46:06 +0100

    Adds missing specs

--- spec/models/api/additional_code_spec.rb
+++ spec/models/api/additional_code_spec.rb
@@ -7,11 +7,39 @@ RSpec.describe Api::AdditionalCode do
                   description: 'COFCO International Argentina S.A.',
                   formatted_description: 'COFCO International Argentina S.A.'
 
+  describe '#formatted_code' do
+    context 'when the additional code type is `excise`' do
+      subject(:additional_code) { build(:api_additional_code, :excise) }
+
+      it { expect(additional_code.formatted_code).to eq('99A') }
+    end
+
+    context 'when the additional code type is not `excise`' do
+      subject(:additional_code) { build(:api_additional_code, :company) }
+
+      it { expect(additional_code.formatted_code).to eq('C490') }
+    end
+  end
+
   describe '#additional_code_type' do
     it { expect(additional_code.additional_code_type).to eq('C') }
   end
 
   describe '#additional_code_id' do
     it { expect(additional_code.additional_code_id).to eq('490') }
+  end
+
+  describe '#excise?' do
+    context 'when the additional code type is `excise`' do
+      subject(:additional_code) { build(:api_additional_code, :excise) }
+
+      it { is_expected.to be_excise }
+    end
+
+    context 'when the additional code type is not `excise`' do
+      subject(:additional_code) { build(:api_additional_code, :company) }
+
+      it { is_expected.not_to be_excise }
+    end
   end
 end
