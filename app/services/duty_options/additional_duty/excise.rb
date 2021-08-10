beb4dabf5a17a33f9ce4747bfcdb51e415b8c2e8
William Fish <william.michael.fish@gmail.com>
2021-08-10 15:52:41 +0100

    Code review

--- app/services/duty_options/additional_duty/excise.rb
+++ app/services/duty_options/additional_duty/excise.rb
@@ -10,16 +10,37 @@ module DutyOptions
         presented_row << I18n.t(
           'duty_calculations.options.excise_duty_html',
           additional_code_description: excise_description,
-        ).html_safe
+        )
 
-        presented_row.concat(duty_evaluation.slice(:calculation, :formatted_value).values)
+        presented_row.concat(evaluation)
+      end
+
+      private
+
+      def evaluation
+        duty_evaluation.values_at(
+          :calculation,
+          :formatted_value,
+        )
       end
 
       def excise_description
-        additional_code = uk_filtered_commodity.applicable_additional_codes.dig(measure.measure_type.id, 'additional_codes').find do |code|
+        additional_code['overlay'].presence ||
+          measure.additional_code.formatted_description.presence ||
+          measure.additional_code.description
+      end
+
+      def additional_code
+        applicable_additional_codes.find do |code|
           code['code'] == "X#{user_session.additional_code_for(measure.measure_type)}"
         end
-        additional_code['overlay'].presence || measure.additional_code.formatted_description.presence || measure.additional_code.description
+      end
+
+      def applicable_additional_codes
+        uk_filtered_commodity.applicable_additional_codes.dig(
+          measure.measure_type.id,
+          'additional_codes',
+        )
       end
     end
   end
