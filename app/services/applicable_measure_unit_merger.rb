d4d2c4b09f47b657bff6be5e8b1a2ec6748c7843
William Fish <william.michael.fish@gmail.com>
2021-08-20 15:36:36 +0100

    Removes merging complexity in merger now that we can ignore the sids

--- app/services/applicable_measure_unit_merger.rb
+++ app/services/applicable_measure_unit_merger.rb
@@ -10,11 +10,7 @@ class ApplicableMeasureUnitMerger
   private
 
   def delta_measure_units
-    uk_filtered_commodity.applicable_measure_units.merge(xi_filtered_commodity.applicable_measure_units) do |_key, uk_units, xi_units|
-      uk_units['measure_sids'] += xi_units['measure_sids']
-
-      uk_units
-    end
+    uk_filtered_commodity.applicable_measure_units.merge(xi_filtered_commodity.applicable_measure_units)
   end
 
   def uk_measure_units
