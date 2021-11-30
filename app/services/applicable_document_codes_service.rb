6679a162cb91478830448de599f1ff563497b03b
William Fish <william.michael.fish@gmail.com>
2021-11-30 11:09:55 +0000

    Fix bug with document codes
    The document codes must be unique and sorted with the last option always
    being 'None of the above'
    
    This commodity for Norway https://staging.trade-tariff.service.gov.uk/xi/commodities/1905320500?country=NO#import
    shows up two None options that aren't at the end of the list and present
    incorrectly in the UI

--- app/services/applicable_document_codes_service.rb
+++ app/services/applicable_document_codes_service.rb
@@ -6,9 +6,13 @@ class ApplicableDocumentCodesService
   end
 
   def call
-    return delta_measure_condition_documents if user_session.deltas_applicable?
+    if user_session.deltas_applicable?
+      delta_measure_condition_documents
+    else
+      send("#{user_session.commodity_source}_measure_condition_documents")
+    end
 
-    send("#{user_session.commodity_source}_measure_condition_documents")
+    @applicable_document_codes
   end
 
   private
@@ -16,29 +20,34 @@ class ApplicableDocumentCodesService
   def delta_measure_condition_documents
     @applicable_document_codes['uk'] = surface_document_codes(uk_filtered_commodity)
     @applicable_document_codes['xi'] = surface_document_codes(xi_filtered_commodity)
-    @applicable_document_codes
   end
 
   def uk_measure_condition_documents
     @applicable_document_codes['uk'] = surface_document_codes(uk_filtered_commodity)
-    @applicable_document_codes
   end
 
   def xi_measure_condition_documents
     @applicable_document_codes['xi'] = surface_document_codes(xi_filtered_commodity)
-    @applicable_document_codes
   end
 
   def surface_document_codes(commodity)
-    commodity.applicable_measures.each_with_object({}) { |measure, acc|
+    document_codes = commodity.applicable_measures.each_with_object({}) do |measure, acc|
       next unless measure.expresses_document?
       next if measure.document_codes.blank?
 
-      acc[measure.measure_type.id] ||= []
-      acc[measure.measure_type.id].concat(measure.document_codes)
-    }.slice(
-      *Api::MeasureType::SUPPORTED_MEASURE_TYPE_IDS,
-    )
+      acc[measure.measure_type.id] ||= Set.new
+      acc[measure.measure_type.id] = acc[measure.measure_type.id] | measure.document_codes
+    end
+
+    document_codes = document_codes.each_with_object({}) do |(measure_type_id, measure_type_document_codes), acc|
+      measure_type_document_codes = measure_type_document_codes.to_a
+      last_option = measure_type_document_codes.delete(code: 'None', description: 'None of the above')
+
+      acc[measure_type_id] = measure_type_document_codes.sort_by { |document_code| document_code[:code] }
+      acc[measure_type_id] << last_option if last_option
+    end
+
+    document_codes.slice(*Api::MeasureType::SUPPORTED_MEASURE_TYPE_IDS)
   end
 
   def user_session
