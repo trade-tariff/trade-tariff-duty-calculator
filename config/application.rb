daf49d524a2c24b0fff53109f4f15d0412646b5c
William Fish <william.michael.fish@gmail.com>
2022-02-17 13:56:10 +0000

    HOTT-1295: Move to rails 6.1 defaults

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ Bundler.require(*Rails.groups)
 
 module TradeTariffDutyCalculator
   class Application < Rails::Application
-    config.load_defaults 5.2
+    config.load_defaults 6.1
     config.middleware.use Rack::Deflater
     config.time_zone                 = 'London'
     config.exceptions_app            = routes
