cff5e24ce906c1de0588f9dedb3c9b23dcffceaf
William Fish <william.michael.fish@gmail.com>
2022-12-06 13:46:56 +0000

    HOTT-2237: Use require_relative

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,8 @@ require 'action_controller/railtie'
 require 'action_view/railtie'
 # require "action_cable/engine"
 # require "rails/test_unit/railtie"
-require 'errors'
+
+require_relative '../lib/errors'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
