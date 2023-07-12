5d3626e9e279c70f113a90d6b6a76e7d3efa9750
Amber Cronin <amber@amber.vision>
2023-07-12 15:47:20 +0100

    HOTT-3621: Fix failing test

--- spec/controllers/healthcheck_controller_spec.rb
+++ spec/controllers/healthcheck_controller_spec.rb
@@ -1,13 +1,13 @@
 RSpec.describe HealthcheckController do
-  describe 'GET #ping' do
-    subject(:response) { get :ping }
+  describe 'GET #healthcheck' do
+    subject(:response) { get :healthcheck }
 
-    it { expect(response.body).to eq('PONG') }
+    it { expect(response.body).to eq('{"git_sha1":"test"}') }
     it { expect(response).to have_http_status(:ok) }
   end
 
-  describe 'GET #healthcheck' do
-    subject(:response) { get :healthcheck }
+  describe 'GET #healthcheckz' do
+    subject(:response) { get :healthcheckz }
 
     it { expect(response.body).to eq('{"git_sha1":"test"}') }
     it { expect(response).to have_http_status(:ok) }
