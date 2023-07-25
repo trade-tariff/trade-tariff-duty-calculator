517666972b206461b703adab1465c294f7c7dece
Amber Cronin <amber@amber.vision>
2023-07-25 13:55:23 +0100

    BAU: Update backend API options

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,7 +1,7 @@
 locals {
   service = "duty-calculator"
   api_service_backend_url_options = {
-    uk = "https://${var.base_domain}/uk/"
-    xi = "https://${var.base_domain}/xi/"
+    uk = "http://backend-uk.tariff.internal:8080"
+    xi = "http://backend-xi.tariff.internal:8080"
   }
 }
