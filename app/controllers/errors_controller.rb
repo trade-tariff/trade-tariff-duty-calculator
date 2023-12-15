66e66a28e39db30314287aafa32e633bfbb44827
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2023-12-15 16:40:48 +0000

    HOTT-4630: Extend DutyCalculator ErrorsController

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -24,4 +24,44 @@ class ErrorsController < ApplicationController
       format.json { render json: { error: 'Unprocessable entity' }, status: :unprocessable_entity }
     end
   end
+
+  def bad_request
+    @skip_news_banner = true
+
+    respond_to do |format|
+      format.html { render status: :bad_request }
+      format.json { render json: { error: 'Bad request' }, status: :bad_request }
+      format.all { render status: :bad_request, plain: 'Bad request' }
+    end
+  end
+
+  def not_implemented
+    @skip_news_banner = true
+
+    respond_to do |format|
+      format.html { render status: :bad_request }
+      format.json { render json: { error: 'Bad request: Not implemented' }, status: :bad_request }
+      format.all { render status: :bad_request, plain: 'Bad request: Not Implemented' }
+    end
+  end
+
+  def method_not_allowed
+    @skip_news_banner = true
+
+    respond_to do |format|
+      format.html { render status: :bad_request }
+      format.json { render json: { error: 'Bad request: Method not allowed' }, status: :bad_request }
+      format.all { render status: :bad_request, plain: 'Bad request' }
+    end
+  end
+
+  def not_acceptable
+    @skip_news_banner = true
+
+    respond_to do |format|
+      format.html { render status: :bad_request }
+      format.json { render json: { error: 'Bad request: Not acceptable' }, status: :bad_request }
+      format.all { render status: :bad_request, plain: 'Bad request' }
+    end
+  end
 end
