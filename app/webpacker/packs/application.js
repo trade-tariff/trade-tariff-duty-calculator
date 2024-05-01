6a40b4f696cfac319cbdf26bbf8a1cc7f5245417
rasika.abeyrathna <rasikasri@gmail.com>
2024-05-01 18:11:58 +0100

    GL-79: Upgrade GovUK Frontend libraries - js issue fix

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -2,7 +2,7 @@ require.context('govuk-frontend/dist/govuk/assets');
 
 import '../styles/application.scss';
 import Rails from 'rails-ujs';
-import { initAll } from 'govuk-frontend';
+import { initAll } from 'govuk-frontend/dist/govuk/govuk-frontend.min.js';
 
 Rails.start();
 initAll();
