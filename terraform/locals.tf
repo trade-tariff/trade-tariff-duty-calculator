b6ccfee80d144d8f79408b31d26f1ac832ea1809
William Fish <william.michael.fish@gmail.com>
2025-03-14 20:16:49 +0000

    HMRC-814: Terraform adjustments
    This change reflects how we now store secrets in the image via
    .env.production
    
    Co-authored-by: Aigul Djon <aigul.djon@gmail.com>

--- terraform/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  service = "duty-calculator"
-  api_service_backend_url_options = {
-    uk = "http://backend-uk.tariff.internal:8080"
-    xi = "http://backend-xi.tariff.internal:8080"
-  }
-  govuk_app_domain = var.environment != "production" ? var.environment == "development" ? "tariff-duty-calculator-dev" : "tariff-duty-calculator-staging" : "tariff-duty-calculator"
-}
--- terraform/main.tf
+++ terraform/main.tf
@@ -24,66 +24,9 @@ module "service" {
   cpu    = var.cpu
   memory = var.memory
 
-  execution_role_policy_arns = [
-    aws_iam_policy.secrets.arn
-  ]
-
   task_role_policy_arns = [
     aws_iam_policy.exec.arn,
   ]
 
-  service_environment_config = [
-    {
-      name  = "PORT"
-      value = "8080"
-    },
-    {
-      name  = "API_SERVICE_BACKEND_URL_OPTIONS"
-      value = jsonencode(local.api_service_backend_url_options)
-    },
-    {
-      name  = "DUTY_CALCULATOR_EXCISE_STEP_ENABLED"
-      value = "true"
-    },
-    {
-      name  = "GOVUK_APP_DOMAIN"
-      value = "${local.govuk_app_domain}.london.cloudapps.digital"
-    },
-    {
-      name  = "GOVUK_WEBSITE_ROOT"
-      value = "https://www.gov.uk"
-    },
-    {
-      name  = "RAILS_ENV"
-      value = "production"
-    },
-    {
-      name  = "RAILS_SERVE_STATIC_FILES"
-      value = "true"
-    },
-    {
-      name  = "RUBYOPT"
-      value = "--enable-yjit"
-    },
-    {
-      name  = "TRADE_TARIFF_FRONTEND_URL"
-      value = "https://${var.base_domain}"
-    },
-    {
-      name  = "GOOGLE_TAG_MANAGER_CONTAINER_ID"
-      value = var.google_tag_manager_container_id
-    }
-  ]
-
-  service_secrets_config = [
-    {
-      name      = "NEW_RELIC_LICENSE_KEY"
-      valueFrom = data.aws_secretsmanager_secret.new_relic_license_key.arn
-    },
-    {
-      name      = "SECRET_KEY_BASE"
-      valueFrom = data.aws_secretsmanager_secret.duty_calculator_secret_key_base.arn
-    },
-  ]
   enable_ecs_exec = true
 }
