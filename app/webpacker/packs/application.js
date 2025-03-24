3a8323bdda62c9805dfa82674db76f455821b426
William Fish <william.michael.fish@gmail.com>
2025-03-24 12:02:57 +0000

    BAU: Bumps js dependencies

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -4,7 +4,5 @@ import '../styles/application.scss';
 import Rails from 'rails-ujs';
 import { initAll } from 'govuk-frontend/dist/govuk/govuk-frontend.min.js';
 
-require('../src/javascripts/cookie-manager.js');
-
 Rails.start();
 initAll();
